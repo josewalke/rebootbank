026978d377c051a932ebe69f8c7a14b0472e5648
Jose Perez <josejperez@singularfactory.net>
2019-11-01 18:25:15 +0000

    fin

--- front/scripts/codigoQR.js
+++ front/scripts/codigoQR.js
@@ -1,30 +1,29 @@
-(function authenticated() {
-    if (localStorage.getItem('token')) {
-        console.log('user authenticated')
-    } else {
-        console.log('user not authenticated')
-    }
+(function authenticated () {
+  if (localStorage.getItem('token')) {
+    console.log('user authenticated')
+  } else {
+    console.log('user not authenticated')
+  }
 })()
 
 const api = axios.create({
-    baseURL: "http://localhost:2222/api/",
-    timeout: 1000
-});
+  baseURL: 'http://localhost:2222/api/',
+  timeout: 1000
+})
 
 api.get('cliente/count', {
-    headers: {
-        token: localStorage.getItem("token")
-    }
+  headers: {
+    token: localStorage.getItem('token')
+  }
 }).then(data => {
-    //console.log(data.data);
-    var count = data.data
-    var span = document.getElementById('count');
-    span.value = count;
+  // console.log(data.data);
+  var count = data.data
+  var span = document.getElementById('count')
+    span.value = count
 
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
-        width: 500,
-        height: 500
-    });
-    var count = document.getElementById('count');
-    qrcode.makeCode("http://localhost:8080//cola?ticket=" + count.value + '&mode=QR');
+    var qrcode = new QRCode(document.getElementById('qrcode'), {
+    width: 500,
+    height: 500
+  })
+    qrcode.makeCode('http://localhost:8080//cola?ticket=' + count.value + '&mode=QR')
 })
