3d4d1593e45cb6d15548284f229709552c9e2fcb
Jose Perez <josejperez@singularfactory.net>
2019-11-01 18:21:35 +0000

    trabajando lo del QR

--- front/scripts/codigoQR.js
+++ front/scripts/codigoQR.js
@@ -1,30 +1,17 @@
-(function authenticated() {
-    if (localStorage.getItem('token')) {
-        console.log('user authenticated')
-    } else {
-        console.log('user not authenticated')
-    }
-})()
-
 const api = axios.create({
-    baseURL: "http://localhost:2222/api/",
-    timeout: 1000
-});
-
-api.get('cliente/count', {
-    headers: {
-        token: localStorage.getItem("token")
-    }
-}).then(data => {
-    //console.log(data.data);
-    var count = data.data
-    var span = document.getElementById('count');
-    span.value = count;
-
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
-        width: 500,
-        height: 500
-    });
-    var count = document.getElementById('count');
-    qrcode.makeCode("http://localhost:8080//cola?ticket=" + count.value + '&mode=QR');
+  baseURL: 'http://localhost:2222/api/',
+  timeout: 1000
 })
+
+api.get('clientes/count')
+  .then(data => {
+  // console.log(data.data)
+    var count = data.data
+    var span = document.getElementById('count')
+    span.value = count
+    var qrcode = new QRCode(document.getElementById('qrcode'), {
+      width: 500,
+      height: 500
+    })
+    qrcode.makeCode('http://localhost:8080//cola?ticket=' + count.value + '&mode=QR')
+  })
