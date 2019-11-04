5c7b1550629822759e8f38c0da6a46cfad6b7578
Jose Perez <josejperez@singularfactory.net>
2019-11-04 14:58:01 +0000

    maquetado de la maquina (falta maquetar el QR)

--- front/scripts/maquina.js
+++ front/scripts/maquina.js
@@ -1,4 +1,4 @@
-/*document.getElementById('btn-enviar')
+document.getElementById('btn-enviar')
   .addEventListener('click', event => {
     const email = document.getElementById('remitente').value
     api
@@ -7,14 +7,20 @@
         console.log(client)
         // window.location.href = './inicio.html'
       })
-  })*/
-/*
+  })
+
 api.qr()
   .then(response => {
     console.log(response)
   })
-*/
+
 document.getElementById('btn-escanerqr').addEventListener('click', (event) => {
   // console.log('codigoQR')
   document.getElementById('inicio').style.display = 'none'
+  document.getElementById('codigoqr').style.display = 'inherit'
+})
+document.getElementById('btn-correo').addEventListener('click', (event) => {
+  // console.log('codigoQR')
+  document.getElementById('inicio').style.display = 'none'
+  document.getElementById('correo').style.display = 'inherit'
 })
